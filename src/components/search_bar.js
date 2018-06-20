d9f0aff9e7bdb6f9ca9dcea8876f54f5dae07802
DIXITA SOHANLAL <ndixitasohanlal@lm-sjn-21018787.paypalcorp.com>
2018-06-20 13:47:32 -0700

    Video browser application v1
    Signed-off-by: DIXITA SOHANLAL <ndixitasohanlal@lm-sjn-21018787.paypalcorp.com>

---
+++ src/components/search_bar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+
+class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { term: ''};
+  }
+  render() {
+      return (
+         <div className="search-bar">
+           <input value={this.state.term}
+               onChange={event => this.onInputChange(event.target.value)}></input>
+         </div>
+      )
+  }
+
+  onInputChange(term) {
+      this.setState({term});
+      this.props.onSearchTermChange(term);
+  }
+}
+
+export default SearchBar;
